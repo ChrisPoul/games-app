f62a896690dfdf5eaf3728b998805b540b0a769d
ChrisPoul <chrispoulsenfernandez@gmail.com>
2023-03-30 09:03:54 -0600

    Now the first food item is placed randomly

--- app/snake/page.tsx
+++ app/snake/page.tsx
@@ -1,14 +1,22 @@
 'use client'
+
+import { useEffect, useState } from "react";
 import SnakeComponent from "./components/Snake"
 import FoodComponent from "./components/Food";
 import { GameObject } from "./models";
-import { useState } from "react";
+import { getRandomInt } from "@/app/common";
 
 export default function GameMap() {
-  const [food, setFood] = useState([
-    new GameObject(8, 8),
-    new GameObject(14, 24)
+  let [food, setFood] = useState([
+    new GameObject(0, 0)
   ])
+
+  useEffect(() => {
+    let initialFoodItem = food[0]
+    initialFoodItem.positionX = getRandomInt(10)
+    initialFoodItem.positionY = getRandomInt(10)
+    setFood([initialFoodItem])
+  }, [])
 
   return (
     <div>

fd3468143cb5af86eb0b7aa9eb23804cafaba2ac
ChrisPoul <chrispoulsenfernandez@gmail.com>
2023-04-14 12:24:15 -0600

    Now the map has a set size

--- app/snake/components/GameBoard.tsx
+++ app/snake/components/GameBoard.tsx
@@ -1,14 +1,50 @@
+import {
+  useState, useEffect
+} from "react"
 import { config } from "@/app/config";
 import Food from "../models/food";
 import Snake from "../models/snake";
 import FoodComponent from "./Food";
 import SnakeComponent from "./Snake";
+import { GameObject } from "../models";
+import GameBoard from "../models/gameBoard";
 
-export default function GameBoardComponent(snake: Snake, food: Food) {
+export default function GameBoardComponent() {
+  let [food, setFood] = useState(new Food(
+    new GameObject(6, 4)
+  ))
+  const [snake, setSnake] = useState(new Snake(
+    new GameObject(0, 0)
+  ))
+  const gameBoard = new GameBoard(snake, food)
+  let newDirection = snake.head.direction
+
+  useEffect(() => {
+    function handleKeyDown(event: any) {
+      const keyPressed: string = event.key
+      if (gameBoard.keyPressedIsValid(keyPressed)) {
+        newDirection = keyPressed
+      }
+    }
+    const interval = setInterval(() => {
+      gameBoard.handleSnakeEatingFood()
+      snake.handleColitionWithItsSelf()
+      snake.update()
+      snake.head.direction = newDirection
+      snake.head.move()
+      setSnake(snake => new Snake(...snake))
+    }, config.milisecondsPerFrame)
+    document.addEventListener('keydown', handleKeyDown);
+
+    return function cleanup() {
+      document.removeEventListener('keydown', handleKeyDown);
+      clearInterval(interval)
+    }
+  }, [snake])
 
   return (
     <div
-      className="content-center bg-black"
+      className="bg-black m-auto relative rounded"
       style={{
         width: config.gameMapWidth * config.gameObjectSize + config.gameObjectSizeUnit,
         height: config.gameMapHeight * config.gameObjectSize + config.gameObjectSizeUnit
--- app/snake/page.tsx
+++ app/snake/page.tsx
@@ -1,51 +1,12 @@
 'use client';
 
-import {
-  useState, useEffect
-} from "react"
-import Snake from "./models/snake";
-import Food from "./models/food";
-import GameBoard from "./models/gameBoard";
-import { GameObject } from "./models";
 import GameBoardComponent from "./components/GameBoard";
-import { config } from "../config";
 
 export default function Page() {
-  let [food, setFood] = useState(new Food(
-    new GameObject(6, 4)
-  ))
-  const [snake, setSnake] = useState(new Snake(
-    new GameObject(0, 0)
-  ))
-  const gameBoard = new GameBoard(snake, food)
-  let newDirection = snake.head.direction
-
-  useEffect(() => {
-    function handleKeyDown(event: any) {
-      const keyPressed: string = event.key
-      if (gameBoard.keyPressedIsValid(keyPressed)) {
-        newDirection = keyPressed
-      }
-    }
-    const interval = setInterval(() => {
-      gameBoard.handleSnakeEatingFood()
-      snake.handleColitionWithItsSelf()
-      snake.update()
-      snake.head.direction = newDirection
-      snake.head.move()
-      setSnake(snake => new Snake(...snake))
-    }, config.milisecondsPerFrame)
-    document.addEventListener('keydown', handleKeyDown);
-
-    return function cleanup() {
-      document.removeEventListener('keydown', handleKeyDown);
-      clearInterval(interval)
-    }
-  }, [snake])
 
   return (
-    <div>
-      {GameBoardComponent(snake, food)}
+    <div className="bg-amber-300 h-screen pt-20">
+      {GameBoardComponent()}
     </div>
   )
 }

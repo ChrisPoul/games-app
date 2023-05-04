4d14887686c3b922485f49095ec97ca01a79ce07
ChrisPoul <chrispoulsenfernandez@gmail.com>
2023-05-04 11:25:12 -0600

    Now there are hot keys for settins menu

--- app/snake/components/GameMenu.tsx
+++ app/snake/components/GameMenu.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import GameOverMenuComponent from "./GameOverMenu"
 import SettingsMenuComponent from "./SettingsMenu"
 
@@ -6,7 +6,14 @@ export default function GameMenuComponent(gameScore: number) {
   const [currentMenu, setCurrentMenu] = useState("none")
   let display = "flex"
   if (currentMenu === "none") { display = "hidden" }
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown)
+  }, [])
 
+  function handleKeyDown(event: KeyboardEvent) {
+    if (event.key == "Escape") { setCurrentMenu("none") }
+    else if (event.key == "s") { setCurrentMenu("settings") }
+  }
   function handleExit() {
     setCurrentMenu("none")
     display = "hidden"
@@ -36,11 +43,14 @@ export default function GameMenuComponent(gameScore: number) {
           </button>
         </div>
       </div>
-      <button className="absolute top-0 right-0 bg-gray-400 p-2 rounded mt-1 mr-1"
-        onClick={openSettingsMenu}
-      >
-        settings
-      </button>
+      {
+        currentMenu == "none" &&
+        <button className="absolute top-0 right-0 bg-gray-400 p-2 rounded mt-1 mr-1"
+          onClick={openSettingsMenu}
+        >
+          settings
+        </button>
+      }
     </div>
   )
 }

4653d9d0d35387ce8f50e666773baaac36d0e0cb
ChrisPoul <chrispoulsenfernandez@gmail.com>
2023-05-08 13:04:25 -0600

    Refactored

--- app/snake/game.ts
+++ app/snake/game.ts
@@ -11,50 +11,32 @@ export function snakeDirectionIsValid(oldDirection: string, newDirection: string
   return true
 }
 
-export function doInterval(
-  gameStatus: string,
-  snakeDirection: string,
-  snake: GameObject[],
-  food: GameObject[]
-) {
-  switch (gameStatus) {
-    case "game-running":
-      handleSnakeEatingFood(snake, food)
-      updateSnakePosition(snake)
-      const snakeHead = snake[0]
-      snakeHead.updatePosition(snakeDirection)
-      break
-    case "game-ending":
-      updateSnakePosition(snake)
-      break
+export function handleSnakeEatingFood(snake: GameObject[], food: GameObject[]) {
+  const snakeHead = snake[0]
+  for (const foodItem of food) {
+    if (snakeHead.collidesWith([foodItem])) {
+      snake.push(new GameObject(0, 0))
+      addNewFoodItem(snake, foodItem)
+      config.milisecondsPerFrame -= config.speedIncrease
+      return
+    }
   }
+  function addNewFoodItem(snake: GameObject[], foodItem: GameObject) {
+    foodItem.positionX = getRandomInt(config.gameMapWidth)
+    foodItem.positionY = getRandomInt(config.gameMapHeight)
+    if (foodItem.collidesWith(snake)) { addNewFoodItem(snake, foodItem) }
+    else if (foodItem.collidesWith(food)) { addNewFoodItem(snake, foodItem) }
+    else return
+  }
+}
 
-  function handleSnakeEatingFood(snake: GameObject[], food: GameObject[]) {
-    const snakeHead = snake[0]
-    for (const foodItem of food) {
-      if (snakeHead.collidesWith([foodItem])) {
-        snake.push(new GameObject(0, 0))
-        addNewFoodItem(snake, foodItem)
-        config.milisecondsPerFrame -= config.speedIncrease
-        return
-      }
-    }
-    function addNewFoodItem(snake: GameObject[], foodItem: GameObject) {
-      foodItem.positionX = getRandomInt(config.gameMapWidth)
-      foodItem.positionY = getRandomInt(config.gameMapHeight)
-      if (foodItem.collidesWith(snake)) { addNewFoodItem(snake, foodItem) }
-      else if (foodItem.collidesWith(food)) { addNewFoodItem(snake, foodItem) }
-      else return
-    }
-  }
-  function updateSnakePosition(snake: GameObject[]) {
-    const snakeBody = snake.slice(1)
-    for (let index = snakeBody.length; index > 0; index--) {
-      let currentBodyPart = snake[index]
-      let nextBodyPart = snake[index - 1]
-      currentBodyPart.positionX = nextBodyPart.positionX
-      currentBodyPart.positionY = nextBodyPart.positionY
-    }
+export function updateSnakePosition(snake: GameObject[]) {
+  const snakeBody = snake.slice(1)
+  for (let index = snakeBody.length; index > 0; index--) {
+    let currentBodyPart = snake[index]
+    let nextBodyPart = snake[index - 1]
+    currentBodyPart.positionX = nextBodyPart.positionX
+    currentBodyPart.positionY = nextBodyPart.positionY
   }
 }
 
--- app/snake/page.tsx
+++ app/snake/page.tsx
@@ -1,62 +1,67 @@
 'use client';
-
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import { config } from "@/app/config";
 import { GameObject } from "@/app/gameObject";
 import {
-  snakeDirectionIsValid, getGameStatus, doInterval
+  snakeDirectionIsValid, getGameStatus, handleSnakeEatingFood, updateSnakePosition
 } from "./game";
 import GameMenuComponent from "./components/GameMenu";
 import GameMapComponent from "./components/GameMap";
 import GameOverScreenComponent from "./components/GameOverScreen";
 
-let snakeDirection: string
-let newSnakeDirection = "Down"
-
 export default function Page() {
+  const snakeDirection = useRef("Down")
+  const newSnakeDirection = useRef("Down")
   const [food, setFood] = useState([
-    new GameObject(
-      Math.floor(config.gameMapWidth / 2), 4
-    ),
-    new GameObject(
-      Math.floor(config.gameMapWidth / 2), 1
-    )
+    new GameObject(Math.floor(config.gameMapWidth / 2), 10),
+    new GameObject(Math.floor(config.gameMapWidth / 2), 14)
   ])
   const [snake, setSnake] = useState([
     new GameObject(Math.floor(config.gameMapWidth / 2), 0)
   ])
   const [gameIsOver, setGameIsOver] = useState(false)
 
+  // handle user input
   useEffect(() => {
-    let gameStatus = getGameStatus(snake)
-    switch (gameStatus) {
-      case "ending":
-        config.milisecondsPerFrame = 100
-        break
-      case "game-over":
-        setGameIsOver(true)
-        return
-    }
-    setTimeout(() => {
-      if (snakeDirectionIsValid(snakeDirection, newSnakeDirection) || snake.length == 1) {
-        snakeDirection = newSnakeDirection
+    function handleKeyDown(event: KeyboardEvent) {
+      const keyPressed = event.key
+      if (keyPressed.includes("Arrow")) {
+        newSnakeDirection.current = keyPressed.replace("Arrow", "")
       }
-      doInterval(gameStatus, snakeDirection, snake, food)
-      setSnake(snake => [...snake])
-    }, config.milisecondsPerFrame)
+    }
     document.addEventListener('keydown', handleKeyDown);
 
     return function cleanup() {
       document.removeEventListener('keydown', handleKeyDown);
     }
+  }, [])
+
+  // run game logic
+  useEffect(() => {
+    const gameStatus = getGameStatus(snake)
+    setTimeout(() => {
+      if (snakeDirectionIsValid(snakeDirection.current, newSnakeDirection.current)) {
+        snakeDirection.current = newSnakeDirection.current
+      }
+      switch (gameStatus) {
+        case "game-running":
+          handleSnakeEatingFood(snake, food)
+          updateSnakePosition(snake)
+          const snakeHead = snake[0]
+          snakeHead.updatePosition(snakeDirection.current)
+          break
+        case "game-ending":
+          updateSnakePosition(snake)
+          config.milisecondsPerFrame = 100
+          break
+        case "game-over":
+          setGameIsOver(true)
+          return
+      }
+      setSnake(snake => [...snake])
+    }, config.milisecondsPerFrame)
   }, [snake])
 
-  function handleKeyDown(event: any) {
-    const keyPressed: string = event.key
-    if (keyPressed.includes("Arrow")) {
-      newSnakeDirection = keyPressed.replace("Arrow", "")
-    }
-  }
   return (
     <div className="bg-amber-300 h-screen pt-6 z--10">
       {GameMapComponent(snake, food)}

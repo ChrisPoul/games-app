fc047b685bf42fd07230420548611f769dd28641
ChrisPoul <chrispoulsenfernandez@gmail.com>
2023-05-12 01:16:12 -0600

    Changed some names and the witdh of act it out

--- app/act-it-out/page.tsx
+++ app/act-it-out/page.tsx
@@ -24,11 +24,11 @@ export default function Page() {
   return (
     <div className=" bg-violet-400 h-screen pt-32">
       <h3 className=" font-extrabold text-5xl fixed top-5 left-9">¡Actúalo!</h3>
-      <div className=" bg-pink-200 border-2 shadow-lg m-auto relative rounded-3xl w-[60%] h-[70vh] text-center">
+      <div className=" bg-pink-200 border-2 shadow-lg m-auto relative rounded-3xl w-[25em] h-[70vh] text-center">
         <img className=" max-h-[65%] max-w-full m-auto mt-7" src={movie.primaryImage?.url} />
         <h1 className=" font-semibold text-xl m-3">{movie.titleText?.text}</h1>
         <h2>{movie.releaseDate?.year}</h2>
-        <button className=" bg-violet-400 rounded m-3 p-3" onClick={setNewMovie}>
+        <button className=" bg-pink-600 rounded m-3 p-3" onClick={setNewMovie}>
           Otra Pelicula
         </button>
       </div>

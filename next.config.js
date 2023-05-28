37108929e7eb097cda47d9cbdd8bd200fc1bec0c
ChrisPoul <chrispoulsenfernandez@gmail.com>
2023-05-27 21:54:24 -0600

    Fixed error with config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
     appDir: true,
   },
 }
-
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
+  disable: process.env.NODE_ENV === 'development',
   skipWaiting: true,
 })
 module.exports = withPWA(nextConfig);

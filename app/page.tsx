2ed19aa28b9c5f23f2444dd2f5b51ec3c7c96707
ChrisPoul <chrispoulsenfernandez@gmail.com>
2023-05-30 11:37:15 -0600

    Modified main menu to look good

--- app/page.tsx
+++ app/page.tsx
@@ -8,48 +8,21 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
+      <div>
         <Image
-          className={styles.logo}
+          className="m-auto mt-9 animate-bounce"
           src="/next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
           priority
         />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
       </div>
 
       <div className={styles.grid}>
         <Link
           href="/snake"
-          className={styles.card}
+          className={styles.card + " animate-pulse"}
         >
           <h2 className={inter.className}>
             Snake <span>-&gt;</span>
@@ -71,9 +44,9 @@ export default function Home() {
           </p>
         </Link>
 
-        <a
+        <Link
           href="/movie-generator"
-          className={styles.card}
+          className={styles.card + " animate-pulse"}
         >
           <h2 className={inter.className}>
             Movie Generator <span>-&gt;</span>
@@ -81,7 +54,7 @@ export default function Home() {
           <p className={inter.className}>
             Un generador de peliculas al azar
           </p>
-        </a>
+        </Link>
       </div>
     </main>
   )

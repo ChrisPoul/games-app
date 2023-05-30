ba619fe0df58c3e614d419dbbf124410faf7af90
ChrisPoul <chrispoulsenfernandez@gmail.com>
2023-05-30 10:36:35 -0600

    Now the image looks good

--- app/movie-generator/page.tsx
+++ app/movie-generator/page.tsx
@@ -26,14 +26,18 @@ export default function Page() {
   }
 
   return (
-    <div className=" bg-violet-400 h-screen">
-      <h3 className=" font-extrabold text-5xl p-5">
+    <div className=" text-center bg-violet-400 h-screen">
+      <h3 className=" font-extrabold text-5xl p-8">
         ¡Actúalo!
       </h3>
+      <div className="relative h-full max-h-[65vh] max-w-[96vw] m-auto">
+        <Image className="object-contain"
+          src={movie.primaryImage.url}
+          alt={movie.titleText.text}
+          fill
+        />
+      </div>
       <div className="text-center p-3">
-        <Image className="max-w-full m-auto mt-7"
-          width={movie.primaryImage.width} height={movie.primaryImage.height}
-          src={movie.primaryImage.url} alt={movie.titleText.text} />
         <h1 className=" font-semibold text-xl m-3">{movie.titleText?.text}</h1>
         <h2>{movie.releaseDate?.year}</h2>
         <button className=" bg-pink-600 rounded m-3 p-3" onClick={setNewMovie}>

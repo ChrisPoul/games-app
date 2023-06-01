b42ee5436ec3e7ef05903d9632dc90ed7aac8992
ChrisPoul <chrispoulsenfernandez@gmail.com>
2023-05-31 22:56:51 -0600

    Refactored the way game object is displayed

--- app/snake/components/GameMap.tsx
+++ app/snake/components/GameMap.tsx
@@ -3,23 +3,19 @@ import GameObjectComponent from "./GameObject";
 
 export default function GameMapComponent(snake: GameObject[], food: GameObject[]) {
   return (
-    <div
-      className="bg-black m-auto relative w-screen h-screen"
-    >
-      <div>
-        {food.map((foodItem, index) => (
-          <div key={"food-" + index}>
-            {GameObjectComponent(foodItem, "bg-red-500")}
-          </div>
-        ))}
-      </div >
-      <div>
-        {snake.map((snakeBodyPart, index) => (
-          <div key={"snake-" + index}>
-            {GameObjectComponent(snakeBodyPart, "bg-green-500")}
-          </div>
-        ))}
-      </div >
+    <div className="bg-black m-auto relative w-screen h-screen">
+      {food.map((foodItem, index) => (
+        <div key={"food-" + index}>
+          <GameObjectComponent className="bg-gray-500"
+            gameObject={foodItem} />
+        </div>
+      ))}
+      {snake.map((snakeBodyPart, index) => (
+        <div key={"snake-" + index}>
+          <GameObjectComponent className="bg-green-500"
+            gameObject={snakeBodyPart} />
+        </div>
+      ))}
     </div>
   )
 }
--- app/snake/components/GameObject.tsx
+++ app/snake/components/GameObject.tsx
@@ -1,10 +1,14 @@
 import { GameObject } from "../gameObject";
 import { config } from "../config";
 
-export default function GameObjectComponent(gameObject: GameObject, gameObjectColor: string) {
+interface GameObjectProps {
+  gameObject: GameObject
+  className: string
+}
+
+export default function GameObjectComponent({ gameObject, className }: GameObjectProps) {
   return (
-    <button
-      className={"rounded absolute " + gameObjectColor}
+    <button className={"rounded absolute " + className}
       style={{
         left: gameObject.X * config.horizontalScaling + "vw",
         top: gameObject.Y * config.verticalScaling + "vh",

2aaf2fa523079807700907543c3327a2122e8b62
ChrisPoul <chrispoulsenfernandez@gmail.com>
2023-06-28 21:48:05 -0600

    Refactored movie generetor into it's own component

--- app/movie-generator/components/Movie.tsx
+++ app/movie-generator/components/Movie.tsx
@@ -1,13 +1,41 @@
-import { Dispatch, SetStateAction } from "react";
+import { useEffect, useRef, useState } from "react";
 import MovieImageComponent from "./MovieImage";
+import { getMoviesRequest } from "../api";
+import SettingsMenuComponent from "./SettingsMenu";
 
-interface MovieProps {
-  movie: Movie
-  imageLoading: boolean
-  setImageLoading: Dispatch<SetStateAction<boolean>>
-}
+export default function MovieComponent() {
+  const moviesCacheRef = useRef<Movie[]>([])
+  const filtersRef = useRef<MovieFilters>({ startYear: 1990 })
+  const index = useRef(0)
+  const [movie, setMovie] = useState<Movie>({
+    titleText: { text: "" },
+    primaryImage: { width: 0, height: 0, url: null },
+    releaseDate: { day: 0, month: 0, year: 0 }
+  })
+  const [imageLoading, setImageLoading] = useState(true)
 
-export default function MovieComponent({ movie, imageLoading, setImageLoading }: MovieProps) {
+  useEffect(() => {
+    refreshMoviesCache()
+  }, [])
+
+  function refreshMoviesCache() {
+    getMoviesRequest(filtersRef.current).then((newMovies) => {
+      moviesCacheRef.current = newMovies
+      setNewMovie()
+    })
+  }
+  function setNewMovie() {
+    if (index.current == moviesCacheRef.current.length - 1) {
+      refreshMoviesCache()
+      index.current = 0
+      return
+    }
+    const movie = moviesCacheRef.current[index.current]
+    index.current += 1
+    setMovie(movie)
+    setImageLoading(true)
+  }
+
   return (
     <div className="grow flex flex-col m-3">
       <MovieImageComponent
@@ -17,6 +45,10 @@ export default function MovieComponent({ movie, imageLoading, setImageLoading }:
       />
       <h1 className="font-semibold text-2xl m-3">{movie.titleText.text}</h1>
       <h2 className="text-lg font-medium">{movie.releaseDate.year}</h2>
+      <button className=" bg-pink-600 rounded p-3 m-auto my-3" onClick={setNewMovie}>
+        Otra Pelicula
+      </button>
+      {SettingsMenuComponent(filtersRef, refreshMoviesCache)}
     </div>
   )
 }
--- app/movie-generator/page.tsx
+++ app/movie-generator/page.tsx
@@ -1,53 +1,10 @@
 "use client"
-import { useEffect, useRef, useState } from "react";
-import { getMoviesRequest } from "./api";
-import SettingsMenuComponent from "./components/SettingsMenu";
 import MovieComponent from "./components/Movie";
 
 export default function Page() {
-  const moviesCacheRef = useRef<Movie[]>([])
-  const filtersRef = useRef<MovieFilters>({ startYear: 1990 })
-  const index = useRef(0)
-  const [movie, setMovie] = useState<Movie>({
-    titleText: { text: "" },
-    primaryImage: { width: 0, height: 0, url: null },
-    releaseDate: { day: 0, month: 0, year: 0 }
-  })
-  const [imageLoading, setImageLoading] = useState(true)
-
-  useEffect(() => {
-    refreshMoviesCache()
-  }, [])
-
-  function refreshMoviesCache() {
-    getMoviesRequest(filtersRef.current).then((newMovies) => {
-      moviesCacheRef.current = newMovies
-      setNewMovie()
-    })
-  }
-  function setNewMovie() {
-    if (index.current == moviesCacheRef.current.length - 1) {
-      refreshMoviesCache()
-      index.current = 0
-      return
-    }
-    const movie = moviesCacheRef.current[index.current]
-    index.current += 1
-    setMovie(movie)
-    setImageLoading(true)
-  }
-
   return (
     <div className="text-center bg-violet-400 dark:bg-black h-screen pt-10 flex flex-col">
-      <MovieComponent
-        movie={movie}
-        imageLoading={imageLoading}
-        setImageLoading={setImageLoading}
-      />
-      <button className=" bg-pink-600 rounded p-3 m-auto mb-6" onClick={setNewMovie}>
-        Otra Pelicula
-      </button>
-      {SettingsMenuComponent(filtersRef, refreshMoviesCache)}
+      <MovieComponent />
     </div>
   )
 }
